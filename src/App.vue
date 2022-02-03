0f1115d224a201f57d828d129090e83c9df23606
Josue <josue.bills@gmail.com>
2022-02-02 23:47:24 -0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <the-header title="RememberMe"></the-header>
+  <!-- <stored-resources :resources="storedResources"></stored-resources> -->
+  <the-resources></the-resources>
+</template>
+
+<script>
+// import LearningResource from './components/learning-resources/LearningResource.vue';
+// import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layout/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
+
+export default {
+  components: {
+    // StoredResources,
+    TheHeader,
+    TheResources
+  }
+};
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
